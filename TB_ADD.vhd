11e2e0a4018bd6bd11c7da49a0ac4ea2298a087b
danipr26 <danielaperez_rojas@utp.edu.co>
2016-10-06 16:02:51 -0500

    Update TB_ADD.vhd

--- TB_ADD.vhd
+++ TB_ADD.vhd
@@ -11,19 +11,19 @@ ARCHITECTURE behavior OF Add_Tb IS
  
     COMPONENT Add
     PORT(
-         Registre1_add : IN  std_logic_vector(6 downto 0);
-         Registre2_add : IN  std_logic_vector(6 downto 0);
-         Registreout_add : OUT  std_logic_vector(6 downto 0)
+         Registre1_add : IN  std_logic_vector(31 downto 0);
+         Registre2_add : IN  std_logic_vector(31 downto 0);
+         Registreout_add : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal Registre1_add : std_logic_vector(6 downto 0) := (others => '0');
-   signal Registre2_add : std_logic_vector(6 downto 0) := (others => '0');
+   signal Registre1_add : std_logic_vector(31 downto 0) := (others => '0');
+   signal Registre2_add : std_logic_vector(31 downto 0) := (others => '0');
 
  	--Outputs
-   signal Registreout_add : std_logic_vector(6 downto 0);
+   signal Registreout_add : std_logic_vector(31 downto 0);
    -- No clocks detected in port list. Replace <clock> below with 
    -- appropriate port name 
  
@@ -43,12 +43,14 @@ BEGIN
    begin		
       -- hold reset state for 100 ns.
       wait for 100 ns;	
-		Registre1_add <= "0010001";
-		Registre2_add <= "1010001";
+		Registre1_add <= "00000000000000000000000000010001";
+		Registre2_add <= "00000000000000000000000001010001";
+		wait for 100 ns;
+		Registre1_add <= "00000000000000000000000000010000";
+		Registre2_add <= "00000000000000000000000001000001";
       -- insert stimulus here 
 
       wait;
    end process;
 
 END;
-
