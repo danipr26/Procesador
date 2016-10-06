39e247e9d37dfb3edf073e156367a2eabc6d1eea
danipr26 <danielaperez_rojas@utp.edu.co>
2016-10-06 16:02:08 -0500

    Update ADD.vhd

--- ADD.vhd
+++ ADD.vhd
@@ -3,9 +3,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Add is
-    Port ( Registre1_add : in  STD_LOGIC_VECTOR (6 downto 0);
-           Registre2_add : in  STD_LOGIC_VECTOR (6 downto 0);
-           Registreout_add : out  STD_LOGIC_VECTOR (6 downto 0));
+    Port ( Registre1_add : in  STD_LOGIC_VECTOR (31 downto 0);
+           Registre2_add : in  STD_LOGIC_VECTOR (31 downto 0);
+           Registreout_add : out  STD_LOGIC_VECTOR (31 downto 0));
 end Add;
 
 architecture ArqAdd of Add is
